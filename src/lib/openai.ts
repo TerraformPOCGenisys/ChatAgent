8aecd2017ce8b96cf3ab371c471e0b5cc1b310f2
Goutham V <gouthamganesh.v@genisys-group.com>
2024-11-25 15:31:41 +0530

    push

---
+++ src/lib/openai.ts
@@ -0,0 +1,6 @@
+import OpenAI from 'openai';
+
+export const openai = new OpenAI({
+  apiKey: "sk-proj-HhTnas_LrWZtv907MN8E0uq2beXvGGLwxltplgX7OqCHMqdyHMpSc8cd5ie664eCh7llxARQBYT3BlbkFJ1m-rQ6w8YLPpcg1_am",
+  dangerouslyAllowBrowser: true
+});
